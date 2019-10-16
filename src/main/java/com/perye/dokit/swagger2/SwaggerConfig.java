c0ebbf8302e40dd7a972454f18870ae53a31c4be
perye <11143526@qq.com>
2019-10-16 11:25:56 +0800

    swagger2配置

--- src/main/java/com/perye/dokit/swagger2/SwaggerConfig.java
+++ src/main/java/com/perye/dokit/swagger2/SwaggerConfig.java
@@ -1,7 +1,21 @@
 package com.perye.dokit.swagger2;
 
+import com.google.common.base.Predicates;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import springfox.documentation.builders.ApiInfoBuilder;
+import springfox.documentation.builders.ParameterBuilder;
+import springfox.documentation.builders.PathSelectors;
+import springfox.documentation.schema.ModelRef;
+import springfox.documentation.service.ApiInfo;
+import springfox.documentation.service.Parameter;
+import springfox.documentation.spi.DocumentationType;
+import springfox.documentation.spring.web.plugins.Docket;
 import springfox.documentation.swagger2.annotations.EnableSwagger2;
+
+import java.util.ArrayList;
+import java.util.List;
 
 /**
  * api页面 /swagger-ui.html
@@ -9,5 +23,37 @@ import springfox.documentation.swagger2.annotations.EnableSwagger2;
 @Configuration
 @EnableSwagger2
 public class SwaggerConfig {
+    @Value("${jwt.header}")
+    private String tokenHeader;
+
+    @Value("${swagger.enabled}")
+    private Boolean enabled;
+
+    @Bean
+    public Docket createRestApi() {
+        ParameterBuilder parameterBuilder = new ParameterBuilder();
+        List<Parameter> parameters = new ArrayList<>();
+        parameterBuilder.name(tokenHeader).description("token")
+                .modelRef(new ModelRef("string"))
+                .parameterType("header")
+                .defaultValue("Bearer")
+                .required(true)
+                .build();
+        parameters.add(parameterBuilder.build());
+        return new Docket(DocumentationType.SWAGGER_2)
+                .enable(enabled)
+                .apiInfo(apiInfo())
+                .select()
+                .paths(Predicates.not(PathSelectors.regex("/error.*")))
+                .build()
+                .globalOperationParameters(parameters);
+    }
+
+    private ApiInfo apiInfo() {
+        return new ApiInfoBuilder()
+                .title("dokit 接口文档")
+                .version("1.0")
+                .build();
+    }
 
 }
