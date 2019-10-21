09f5586f9fd1ba5896e8eabb7f5a9835926f7b6e
perye <11143526@qq.com>
2019-10-21 09:43:03 +0800

    update

--- dokit-front/config/index.js
+++ dokit-front/config/index.js
@@ -33,7 +33,7 @@ module.exports = {
 
     // Various Dev Server settings
     host: 'localhost', // can be overwritten by process.env.HOST
-    port: 8013, // can be overwritten by process.env.PORT, if port is in use, a free one will be determined
+    port: 8000, // can be overwritten by process.env.PORT, if port is in use, a free one will be determined
     autoOpenBrowser: true,
     errorOverlay: true,
     notifyOnErrors: false,
