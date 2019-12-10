22694e746bc4fdb2b082d2dfb37be35670df62a6
perye <11143526@qq.com>
2019-12-10 17:11:23 +0800

    解决查询时输入 []{} 符号报错的问题

--- src/main/java/com/perye/dokit/DokitApplication.java
+++ src/main/java/com/perye/dokit/DokitApplication.java
@@ -3,6 +3,9 @@ package com.perye.dokit;
 import com.perye.dokit.utils.SpringContextHolder;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.embedded.tomcat.TomcatConnectorCustomizer;
+import org.springframework.boot.web.embedded.tomcat.TomcatServletWebServerFactory;
+import org.springframework.boot.web.servlet.server.ServletWebServerFactory;
 import org.springframework.context.annotation.Bean;
 import org.springframework.scheduling.annotation.EnableAsync;
 import org.springframework.transaction.annotation.EnableTransactionManagement;
@@ -19,6 +22,18 @@ public class DokitApplication {
     @Bean
     public SpringContextHolder springContextHolder() {
         return new SpringContextHolder();
+    }
+
+    /**
+     * 解决查询时输入 []{} 符号报错的问题
+     */
+    @Bean
+    public ServletWebServerFactory webServerFactory() {
+        TomcatServletWebServerFactory tomcatServletWebServerFactory = new TomcatServletWebServerFactory();
+        tomcatServletWebServerFactory.addConnectorCustomizers(
+                (TomcatConnectorCustomizer) connector -> connector.setProperty("relaxedQueryChars", "[]{}")
+        );
+        return tomcatServletWebServerFactory;
     }
 
 }
